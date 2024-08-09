a1ac7e8bc7cb559b0551cc89773f08bbd9d3e95c
DESKTOP-R4TO9PD\Administrator <levanle5@gmail.com>
2024-08-10 02:00:37 +0700

    update images

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa4266a61c2e841fe9e0aae056e7ad8f-1559946562.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
